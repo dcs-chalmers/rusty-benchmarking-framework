4d0f1f31eeaf4105ffbc03ec83f15ef8f3182449
Gustav Seffel <seffel@student.chalmers.se>
2025-01-23 15:34:39 +0100

    added config to be able to decide different arguments by themselves while running the benchmark, also added a small readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Lock-free benchmarking tool
+This is a project to test different implementations of lock-free based data structures to measure their output and performance.
+
+## How to use:
+To run the benchmark, 
+* download the the repo
+* Write in a terminal `run cargo` for standard values
+* To use specific values you can add different flags to the run command:
+    * `-t` for specific time values
+    * `-p` for specified amount of producers
+    * `-c` for specified amount of consumers
+
+So for all flags active, write:
+`cargo run -- -t *specified value* -p *specified amount* -c *specified amount*`
+
+# TODO
+* Fix different implementations for queues
+* Add to config to be able to choose which queue to test
--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,38 @@ use std::{
     sync::atomic::{AtomicUsize, AtomicBool, Ordering},
     sync::Barrier
 };
+
+use clap::Parser;
+
 pub mod queues;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Duration of test
+    #[arg(short, long, default_value_t = 10)]
+    time_limit: u64,
+    #[arg(short, long, default_value_t = 20)]
+    producers: usize,
+    #[arg(short, long, default_value_t = 20)]
+    consumers: usize,
+}
 
 pub fn start_benchmark() {
     let test_q: LFQueue<i32> =  LFQueue {
         lfq: lockfree::queue::Queue::new(),
     };
+    let args = Args::parse();
 
-    let time_limit: u64 = 10;
-    let barrier = Barrier::new(20 + 20 + 1);
+
+    let time_limit: u64 = args.time_limit;
+    let barrier = Barrier::new(args.consumers + args.producers + 1);
     let pops  = AtomicUsize::new(0);
     let pushes = AtomicUsize::new(0);
     let done = AtomicBool::new(false);
 
 
-    let thread_count: i32 = 40;
+    // let thread_count: i32 = 40;
     
     thread::scope(|s| {
         let queue = &test_q;
@@ -26,52 +43,56 @@ pub fn start_benchmark() {
         let pops = &pops;
         let done = &done;
         let barrier = &barrier;
+        let consumers = &args.consumers;
+        let producers = &args.producers;
 
-        for i in 0..thread_count{
+        for _ in 0..*producers{
             s.spawn(move || {
-                println!("Thread: {}, working", i);
+                // println!("Thread: {}, working", i);
                 let handle = queue.register();
-                if i % 2 == 0 {
-                    //push
-                    let mut l_pushes = 0; 
-                    barrier.wait();
-                    while !done.load(Ordering::Relaxed) {
-                        handle.push(i);
-                        l_pushes += 1;
-                        
-                        // println!("Thread: {}, pushed!", i);
-                    }
-                    pushes.fetch_add(l_pushes, Ordering::Relaxed);
+                // push
+                let mut l_pushes = 0; 
+                barrier.wait();
+                while !done.load(Ordering::Relaxed) {
+                    handle.push(1);
+                    l_pushes += 1;
+                    
+                    // println!("Thread: {}, pushed!", i);
                 }
-                else {
-                    //pop
-                    let mut l_pops = 0; 
-                    barrier.wait();
-                    while !done.load(Ordering::Relaxed) {
-                        handle.pop();
-                        l_pops += 1;
-                        
-                        // println!("Thread: {}, popped!", i);
-                    }
-                    pops.fetch_add(l_pops, Ordering::Relaxed);
-                }
+                pushes.fetch_add(l_pushes, Ordering::Relaxed);
             }); 
         }
-        println!("Waiting");
+        for _ in 0..*consumers {
+            s.spawn(move || {
+                // println!("Thread: {}, working", i);
+                let handle = queue.register();
+                    // pop
+                let mut l_pops = 0; 
+                barrier.wait();
+                while !done.load(Ordering::Relaxed) {
+                    handle.pop();
+                    l_pops += 1;
+                    
+                    // println!("Thread: {}, popped!", i);
+                }
+                pops.fetch_add(l_pops, Ordering::Relaxed);
+            }); 
+        }
+        // println!("Waiting");
         barrier.wait();
-        println!("Sleeping");
+        // println!("Sleeping");
         std::thread::sleep(std::time::Duration::from_secs(time_limit));
-        println!("Done");
+        // println!("Done");
         done.store(true, Ordering::Relaxed);
-        println!("After done");
+        // println!("After done");
     });
-    println!("into inner pops");
+    // println!("into inner pops");
     let pops = pops.into_inner();
-    println!("into inner pushes");
+    // println!("into inner pushes");
     let pushes = pushes.into_inner();
 
     println!(
-        "Throghput: {}",
+        "Throughput: {}",
         (pushes + pops) as f64 / time_limit as f64
     );
     println!("Number of pushes: {}", pushes);
