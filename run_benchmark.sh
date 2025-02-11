fe6aacdb768462a3a1cc4269a35c8fdb9aed18e1
William Berg <william.berg2@gmail.com>
2025-02-11 10:06:02 +0100

    Rewrote script to just run benchmark on all queues

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-mkdir -p output
-
-if [ $# -eq 0 ]; then 
-    time cargo run --release
-else
-    time cargo run --release -- $1
-fi
+cargo run --release --features memory_tracking,basic_queue -- -t 1 --benchmark pingpong
+cargo run --release --features memory_tracking,array_queue -- -t 1 --benchmark pingpong
+cargo run --release --features memory_tracking,lockfree_queue -- -t 1 --benchmark pingpong
+cargo run --release --features memory_tracking,concurrent_queue -- -t 1 --benchmark pingpong
+cargo run --release --features memory_tracking,bounded_ringbuffer -- -t 1 --benchmark pingpong
