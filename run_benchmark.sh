c455bbd6f8a7017f35eed8c414cd9bc83d3da4c1
William Berg <william.berg2@gmail.com>
2025-02-12 13:31:03 +0100

    Changed so script cleans output folder first, runs bat last.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./clean.sh
 cargo run --release --features memory_tracking,basic_queue -- -t 1 ping-pong
 cargo run --release --features memory_tracking,basic_queue -- -t 1 basic 
 cargo run --release --features memory_tracking,array_queue -- -t 1 ping-pong
@@ -10,3 +11,4 @@ cargo run --release --features memory_tracking,concurrent_queue -- -t 1 ping-pon
 cargo run --release --features memory_tracking,concurrent_queue -- -t 1 basic 
 cargo run --release --features memory_tracking,bounded_ringbuffer -- -t 1 ping-pong
 cargo run --release --features memory_tracking,bounded_ringbuffer -- -t 1 basic 
+bat output/*
