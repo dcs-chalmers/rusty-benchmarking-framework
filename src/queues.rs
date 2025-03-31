aa755c7ba2b78dc6cde4130e3aea586e97ea385f
Gustav Seffel <89899974+GSeffel@users.noreply.github.com>
2025-03-31 14:19:14 +0200

    Update queues.rs

--- src/queues.rs
+++ src/queues.rs
@@ -44,8 +44,5 @@ pub mod seg_queue;
 pub mod tsigas_zhang_queue;
 #[cfg(feature = "tz_queue_hp")]
 pub mod tsigas_zhang_queue_hp;
-<<<<<<< HEAD
 #[cfg(feature = "bbq")]
 pub mod bbq;
-=======
->>>>>>> origin/main
