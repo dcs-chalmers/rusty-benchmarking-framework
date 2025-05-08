c099ca6e86419d5832c5512616c1c917530c394c
William <william.berg2@gmail.com>
2025-05-08 11:52:34 +0200

    Changed BBQ to a non-blocking version.

--- src/queues/bbq.rs
+++ src/queues/bbq.rs
@@ -32,7 +32,7 @@ impl<T: Default> Handle<T> for BBQHandle<'_, T> {
     }
 
     fn pop(&mut self) -> Option<T> {
-        Some(self.queue.queue.pop().expect("failed pop"))
+        self.queue.queue.pop()
     }
 }
 
@@ -45,5 +45,13 @@ mod tests {
         let q: BBQueue<i32> = BBQueue::new(100);
         q.queue.push(1);
         assert_eq!(q.queue.pop().unwrap(), 1);
+    }
+    #[test]
+    fn test_order() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let q: BBQueue<i32> = BBQueue::new(100000);
+        if crate::order::benchmark_order_i32(q, 20, 5, true, 10).is_err() {
+            panic!();
+        }
     }
 }
