86b46ee840e9d8c6555fb230007892ee5e22a50e
Emil Björnlinger <15017341+emilbjornlinger@users.noreply.github.com>
2026-02-03 15:44:51 +0100

    Change run_benchmark script to valid invocations

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 ./clean.sh
-RUST_LOG=debug cargo run --release --features memory_tracking -p basic_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p basic_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p array_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p array_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p lockfree_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p lockfree_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p unbounded_concurrent_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p unbounded_concurrent_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p bounded_ringbuffer -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p bounded_ringbuffer -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p atomic_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p atomic_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p wf_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p wf_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p scc_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p scc_queue -- -t 1 basic
-RUST_LOG=debug cargo run --release --features memory_tracking -p scc2_queue -- -t 1 ping-pong
-RUST_LOG=debug cargo run --release --features memory_tracking -p scc2_queue -- -t 1 basic
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p basic_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p basic_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p array_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p array_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p lockfree_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p lockfree_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p unbounded_concurrent_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p unbounded_concurrent_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p bounded_ringbuffer -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p bounded_ringbuffer -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p atomic_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p atomic_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p wf_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p wf_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p scc_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p scc_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p scc2_queue -- -t 1 prod-con
+RUST_LOG=debug cargo run --release --features benchmark_core/memory_tracking -p scc2_queue -- -t 1 prod-con
 bat output/*
