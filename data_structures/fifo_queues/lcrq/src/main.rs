a9ed612d0fae374a2a8610ef463685bba9bbeb03
Emil Björnlinger <15017341+emilbjornlinger@users.noreply.github.com>
2026-02-05 16:21:06 +0100

    Rename queues to fifo_queues

---
+++ data_structures/fifo_queues/lcrq/src/main.rs
@@ -0,0 +1,21 @@
+use log::*;
+use benchmark_core::benchmarks::fifo_queue_benchmarks::benchmark_fifo_queue;
+
+
+fn main() {
+    // initialize env_logger if not in silent release mode
+    #[cfg(not(all(not(debug_assertions), feature = "silent-release")))]
+    {
+        env_logger::init();
+        debug!("envlogger init");
+    }
+    log::info!("Starting benchmark");
+
+    match benchmark_fifo_queue::<lcrq::LCRQueue<usize>>("array_queue") {
+        Ok(_) => println!("Benchmark done."),
+        Err(e) => {
+            eprintln!("Benchmark received error: {}", e);
+            println!("Benchmark exiting due to error.");
+        }
+    }
+}
